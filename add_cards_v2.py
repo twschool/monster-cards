fab88cff3200d9d3e06b85c245f36118f6d17fe5
twschool <98859425+twschool@users.noreply.github.com>
2023-05-10 10:23:03 +1200

    Add cards is finished

--- add_cards_v2.py
+++ add_cards_v2.py
@@ -71,10 +71,52 @@ creature_dict = {
     }
 }
 
+def nonecheck(user_input):
+    if user_input is None:
+        return True
+    else:
+        return False
 
-enterbox_fields = ["Creature Name"]
-enterbox_values = ["Strength", "Speed", "Stealth", "Cunning"]
-for stat in enterbox_values:
-    enterbox_fields.append(f"{stat}")
 
-new_card_data = eg.multenterbox(fields=enterbox_fields, msg="Add custom card (Stat values must be between 1-25)")
+def add_cards():
+    enterbox_fields = ["Creature Name"]
+    enterbox_values = ["Strength", "Speed", "Stealth", "Cunning"]
+    for stat in enterbox_values:
+        enterbox_fields.append(f"{stat}")
+    enterbox_title = "edit card data"
+    new_card_data = eg.multenterbox(fields=enterbox_fields,
+                                    msg="Add custom card (Stat values must be between 1-25)")
+    if nonecheck(new_card_data):
+        return [False, "User exited out of edit statement"]
+    while True:
+        enterbox_msg = "New card data entry"
+        data_backup = new_card_data
+        new_card_data = eg.multenterbox(fields=enterbox_fields,
+                                        msg=enterbox_msg,
+                                        values=new_card_data)
+        if nonecheck(new_card_data):
+            does_exit = eg.buttonbox(msg="Do you want to exit without saving changes",
+                                    choices = ["Yes", "No"])
+            if does_exit == "Yes":
+                return [None, "User exited without saving"]
+            else:
+                # If user mistakenly cancels then they can go back
+                new_card_data = data_backup
+                continue
+        else:
+            # If user didnt raise a error then exit out of this function
+            for value in new_card_data[1:]:
+                if value == "" or new_card_data[0] == "":
+                    eg.msgbox("No values can be left empty")
+                    continue
+                try:
+                    converted = int(value)
+                except ValueError:
+                    eg.msgbox("All values have to be a integer (apart from card name)")
+                    continue
+            break
+                                 
+    print(new_card_data)
+
+
+add_cards()
